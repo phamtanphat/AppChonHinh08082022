d32eb5167ff1c0048413945cad762a4ad8952ab2
Phat <phatdroid94@gmail.com>
2022-10-19 20:15:20 +0700

    tablelayout

---
+++ app/src/main/java/com/example/appchonhinh08082022/ListImageActivity.java
@@ -0,0 +1,14 @@
+package com.example.appchonhinh08082022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_image);
+    }
+}
